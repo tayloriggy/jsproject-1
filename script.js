ca22c11026c5556475cd74b9fdbd7fe867d898ad
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-10-04 21:45:26 -0500

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+Project 1: Build a simple counter.
+
+Requirements:
+-Create a Github repository for this project. 
+-The count starts at 0
+-Have two buttons on the screen, one to increase (+) the count by 1, and one button to lower (-) the count by 1.
+-Pressing the plus button increases the count by 1, pressing the minus button decreases by 1
+-Create a Github repository for this project and write Git commits as you go.
+
+Steps:
+1. This will be webpage, thus need to create an index.html, style.css, script.js.
+2. Will need to utilize buttons in html.
+3. For JS, utilize an array to increment count.
+
+*/
+
+function counter1 () {
+
+}
+
+function counter2 () {
+    
+}
